e360fc72d8e2161becddc78dada52ccd4e584fe5
VikasGurjar <r9427173766@gmail.com>
2025-10-06 05:13:35 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-5xl rounded-2xl absolute bg-amber-800 '>vikas op in the chat</div>
+  )
+}
+
+export default App ;
